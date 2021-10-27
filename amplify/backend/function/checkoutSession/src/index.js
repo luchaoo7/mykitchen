906e31fa003f4ccee2a60e1fe813cee67973104a
Marcos Maidana <lucha0075.db@gmail.com>
2021-10-27 23:04:41 +0100

    import axios

--- amplify/backend/function/checkoutSession/src/index.js
+++ amplify/backend/function/checkoutSession/src/index.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from "react";
+const axios = require('axios').default;
 
 exports.handler = async (event) => {
     // TODO implement
+
     const response = {
         statusCode: 200,
     //  Uncomment below to enable CORS requests
