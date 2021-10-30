3510dd8fe76519c520d861ddbbf4b11d7e74e38d
Marcos Maidana <lucha0075.db@gmail.com>
2021-10-30 21:31:16 +0100

    Title change

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -5,7 +5,7 @@ const MyJumbotron = () => {
     return (
         <div className="jumbotron">
             <div className="container text-center">
-                <h1>Kitchen Shop</h1>      
+                <h1>The Mobile Casa</h1>      
                 <p>Mission, Vission & Values</p>
             </div>
         </div>
