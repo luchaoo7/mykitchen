ebfeede218df7a8b815d567a94749597012b3cc9
Marcos Maidana <lucha0075.db@gmail.com>
2021-10-31 15:39:21 +0000

    fixing

--- src/components/KitchenContainer.js
+++ src/components/KitchenContainer.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect} from "react";
 //import MyCarousel from '../components/Carousel';
 import NavBar from './NavBar'
 import MyJumbotron from './Jumbotron';
