758265945de0e3d19ef2ee499d89ac2f15171491
Marcos Maidana <lucha0075.db@gmail.com>
2021-11-09 00:01:06 +0000

    add cart button from item

--- src/components/Item.js
+++ src/components/Item.js
@@ -14,22 +14,29 @@ const Item = (props) => {
         height: "150px",
     }
 
+    function addToCart () {
+        alert("Added to cart");
+    }
+
 
     const {name, price, image} = props.product;
     const public_image = process.env.PUBLIC_URL;
     return (
 
         <div className="col-sm-4">
-            <Link to={`/details/${name}`}>
                 <div className="panel panel-primary">
                     <div className="panel-heading">{name}</div>
                     <div className="panel-body">
+                    <Link to={`/details/${name}`}>
                         <img src={`${public_image}/images/${image}`} className="img-responsive" style={style}  alt=""/>
+                    </Link>
                     </div>
                     <div className="panel-footer">Price: £{price}
+                        <button onClick={addToCart} type="button" style={{float:'right'}} className="btn-primary glyphicon glyphicon-shopping-cart">
+                            +
+                        </button>
                     </div>
                 </div>
-            </Link>
         </div>
     )
 }
